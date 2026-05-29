72d125a3e2f59dd5b5782d54f8576c9e75577763
Fahyio <sashailyushin16@gmail.com>
2026-05-29 13:29:54 +0300

    Update settings.py

--- bulletin_board/settings.py
+++ bulletin_board/settings.py
@@ -87,7 +87,8 @@ STATICFILES_DIRS = [BASE_DIR / 'static']
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR / 'media'
+MEDIA_ROOT = os.getenv('MEDIA_ROOT', BASE_DIR / 'media')
+
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
